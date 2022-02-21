bd62ca05c7e555585ac06a757347cdf9b5e0887a
Radka Krenova <radka.krenov@gmail.com>
2022-02-21 19:46:27 +0100

    Lightbox photogallery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-(function($) {
-    $(function() {
-         
+(function($){
+    $(function(){
+    /* Scroll to sections */
+    $(".jq--scroll-our-pizza").click(function(){
+    $("html, body").animate({scrollTop: $(".jq--our-pizza").offset().top}, 1000);
     });
-})(jQuery);
+    });
+    })(jQuery);
